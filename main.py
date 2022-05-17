89b2f544ee9329f37fdd5f964b4bdcbcbc6220d4
Tiago Chaves <tiagodevch@gmail.com>
2022-05-17 00:23:45 -0300

    Configurado upload
    Signed-off-by: Tiago Chaves <tiagodevch@gmail.com>

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+UPLOAD_FOLDER = os.path.join(os.getcwd() + '/templates/static/files/')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])
 def upload():
+    if request.method == 'POST':
+        file = request.files['file']
+        filename = file.filename
+        print(filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER']) + filename)
+        return filename, 200
     return render_template('upload.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
